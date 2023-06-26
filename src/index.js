24424a70142e564f977bfca845d3e22b967bfa25
WinnySigilai <winnysigilai02@gmail.com>
2023-06-26 23:30:02 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let input = document.querySelector("#create-task-form");
+  input.addEventListener("submit", function(e){
+    e.preventDefault();
+    addTodo(e.target.new_task_description.value);
+    e.target.reset();
+  })
 });
+
+function addTodo(todo){
+  const ul = document.querySelector("#tasks");
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn .addEventListener("click", HandleDelete);
+
+  btn.textContent = "X";
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  ul.appendChild(li);
+}
+
+function HandleDelete(e){
+  e.target.parentNode.remove();
+}
